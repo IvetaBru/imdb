263f466ba059d490ce8475cd1e4ba57b6ec8f3ee
IvetaBru <ivetamarkeviciute@gmail.com>
2025-04-14 10:15:30 +0300

    created MoviesContext and types

---
+++ imdb_project/src/contexts/MoviesContext.tsx
@@ -0,0 +1,70 @@
+import { createContext, useReducer, useEffect } from "react";
+
+import { ChildrenProp, Movie, MoviesContextTypes } from "../types";
+
+type ActionTypes = 
+{ type: 'setData', data: Movie[]} |
+{ type: 'addMovie', newMovie: Movie } 
+
+const reducer = (state: Movie[], action: ActionTypes) => {
+    switch(action.type){
+        case 'setData':
+            return action.data;
+        case 'addMovie':
+            return [...state, action.newMovie]
+    }
+}
+
+const MoviesContext = createContext<undefined | MoviesContextTypes>(undefined);
+const MoviesProvider = ({ children }: ChildrenProp) => {
+
+    const [movies, dispatch] = useReducer(reducer, []);
+
+    const addNewMovie = (newMovie: Movie) => {
+        fetch(`http://localhost:8080/movies`, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(newMovie)
+        });
+        dispatch({
+            type:'addMovie',
+            newMovie: newMovie
+        })
+    }
+
+    const findProduct = (id: Movie['id']): Movie | string => {
+        const foundMovie = movies.find(movie => movie.id === id);
+        if(foundMovie){
+            return foundMovie;
+        }else{
+            return 'Error: movie not found';
+        }
+    }
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/movies`)
+        .then(res => res.json())
+        .then((data: Movie[]) => dispatch({
+            type:'setData',
+            data: data
+        }));
+    }, []);
+
+    return (
+
+        <MoviesContext.Provider
+            value={{
+                movies,
+                addNewMovie,
+                findProduct
+            }}
+        >
+            { children }
+        </MoviesContext.Provider>
+    )
+}
+
+export { MoviesProvider };
+export default MoviesContext;
---
+++ imdb_project/src/types.tsx
@@ -0,0 +1,73 @@
+export type ChildrenProp = {
+    children: React.ReactElement
+}
+
+type IMDBRating = {
+    score: number,
+    votes: number
+}
+
+type IMDBData = {
+    totalScore: number,
+    userRatings: IMDBRating[]
+}
+
+type MoviePopularity = {
+    ranking: number,
+    weeklyChange: number
+}
+
+type MoviePhotos = {
+    poster: string[],
+    cutscenes: string[]
+}
+
+type MovieVideos = {
+    trailers: string[],
+    cutscenes: string[]
+}
+
+type MovieWriters = {
+    name: string,
+    role: string
+}
+
+type MovieActors = {
+    name: string,
+    character: string[],
+    actorPhoto: string
+}
+
+type CastAndCrew = {
+    director: string,
+    writers: MovieWriters,
+    actors: MovieActors
+}
+
+type MovieReviews = {
+    users: number,
+    critics: number,
+    metascore: number
+}
+
+export type Movie = {
+    id: string,
+    title: string,
+    releaseYear: number,
+    eirinCategory: string,
+    length: number,
+    IMDB: IMDBData,
+    popularity: MoviePopularity,
+    photos: MoviePhotos,
+    videos: MovieVideos,
+    genres: string[],
+    description: string,
+    castAndCrew: CastAndCrew,
+    reviews: MovieReviews
+}
+
+export type MoviesContextTypes = {
+    movies: Movie[],
+    addNewMovie: (newMovie: Movie) => void,
+    findProduct: (id: Movie["id"]) => Movie | string
+}
