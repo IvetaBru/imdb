b9fa49e27c4d6da0dd92c2d8b27eceda9a3309ea
Paulius <p.jacikeviciusinfo@gmail.com>
2025-04-15 09:14:15 +0300

    Card and HomePage visuals

--- imdb_project/src/components/UI/molecules/MovieCard.tsx
+++ imdb_project/src/components/UI/molecules/MovieCard.tsx
@@ -1,9 +1,157 @@
+import styled from "styled-components";
+import {Link, useNavigate} from "react-router";
+import {Movie} from "../../../types";
+
+import { TurnedIn, Add, Star, StarOutline  } from '@mui/icons-material';
+
+const Flag = styled(TurnedIn)`
+  &.override
+  { 
+    font-size: 60px;
+    position: absolute;
+    bottom: 0;
+    top:  0;
+
+    margin-top: -8px;
+    margin-left: -13px;    
+
+    opacity: 0.8;
+    color: rgba(25,29,28,255);
+  }
+`;
+
+const AddSymbol = styled(Add)`
+  &.override
+  { 
+    font-size: 25px;
+    position: absolute;
+    bottom: 0;
+    top:  0;
+    
+    margin-top: 6px;
+    margin-left: 5px;    
+
+    opacity: 0.8;
+    color: white;
+  }
+`;
+
+
+const StyledMovieCard = styled.div`
+ #container
+ { 
+  width: 180px;
+  border: 1px solid white;
+  border-radius: 5px;
+  flex: 1;
+
+  position: relative;
+
+  .blue 
+  {
+    color: #0ab3dd;
+  }
+  > a
+  {
+    text-decoration: none;
+    color: white;
+
+    > img{
+      width: 100%;
+      height: 267.583px;
+      object-fit: cover;
+    }
+  }  
+  
+  > .filmName 
+  {
+    margin-left: 18px;
+    margin-top: 12px;
+  }
+  > div.stars
+    {
+      display: flex;
+      justify-content: space-evenly;
+      > div
+      {
+        display: flex;      
+        align-items: center;
+        gap: 8px;
+        padding-right: 15px;
+        margin-right: 30px;
+
+        > .star 
+        {
+          color: yellow;
+        }
+
+        > h3 
+        {
+          margin: 0;
+        }
+      }
+
+      > .starEmpty
+      {        
+        color: lightblue;
+      }
+    }
+
+  > div.more
+    { 
+      display: flex;
+      justify-content: center;
+      margin-top: 12px;
+
+        > #button
+        {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: rgba(44,44,44,255);
+          border-radius: 75px;
+          height: 40px;
+          width: 150px;
+          margin-bottom: 12px;
+        }
+    }
+ }
+`;
+
 const MovieCard = () => {
-    return ( 
-       <>
-       
-       </>
-     );
+  return ( 
+    <StyledMovieCard>
+      <div id="container">
+        <Flag className={'override'}/>
+        <AddSymbol className={'override'}/>
+        
+        <a href="">
+          <img src="http://pngimg.com/d/mario_PNG125.png" alt="" />
+        </a>
+
+        <div className="stars">
+          <div>
+            <Star className={'star'}/>
+            <h3>6.6</h3>              
+          </div>
+          <StarOutline className={'starEmpty'}/>  
+        </div>
+
+        <a href="" className="filmName">Linkas</a>
+
+        <div className="more">
+          <div id="button">      
+            <Add className={'blue'}/>
+            <span className="blue">Watchlist</span>
+          </div>
+
+          <div className="trailer">
+          </div>
+
+        </div>
+      </div>
+    </StyledMovieCard>
+  );
 }
  
 export default MovieCard;
--- imdb_project/src/components/pages/Home.tsx
+++ imdb_project/src/components/pages/Home.tsx
@@ -1,8 +1,79 @@
+import { useContext } from "react";
+import styled from "styled-components";
+import MovieCard from "../UI/molecules/MovieCard";
+import { ArrowForwardIos } from '@mui/icons-material';
+
+// TODO context
+// import MerchContext from "../../contexts/MerchContext";
+
+const StyledSection = styled.section`
+ 
+  padding: 0 18px;
+  margin: 30px 0;
+  
+  > .headerContainer
+  {
+    display: flex;
+    gap: 5px;
+    margin: 16px 0;
+    padding: 0 16px;
+
+  }
+
+  > .cardsContainer
+  {
+    display: flex;
+    padding: 0 16px;
+    gap: 15px;
+  }
+
+`;
+
+const StyledHeader = styled.h3`
+  border-left: 6px solid yellow;
+  height: auto;
+  padding: 0 12px;  
+  margin: 0; 
+`;
+
+const Arrow = styled(ArrowForwardIos)`
+  &.override
+  {
+    margin-top: 3px;
+  }
+`;
+
 const Home = () => {
+
+    // TODO: context
+    // const { merch } = useContext(MerchContext)!;
+    // const { merch } = useContext(MerchContext) as MerchContextTypes;
+   
     return ( 
-       <>
-       
-       </>
+      <>
+        <StyledSection>
+          {/* <h3 className="title">Top Picks</h3> */}
+          <div className="headerContainer">
+          <StyledHeader>Top Picks</StyledHeader>
+          <Arrow className={'override'} />
+          </div>
+          
+          <div className="cardsContainer">
+            <MovieCard/>
+            <MovieCard/>
+          </div>
+        </StyledSection>
+        <StyledSection>
+         <div className="headerContainer">
+            <StyledHeader>Top Picks</StyledHeader>
+            <Arrow className={'override'} />
+          </div>
+          <div className="cardsContainer">
+            <MovieCard/>
+            <MovieCard/>
+          </div>
+        </StyledSection>
+      </>   
      );
 }
  
